26e437e60d402532dd76a7f2bf21d0d0a952d953
Davi de Souza Farias <169002831+DSFNEW@users.noreply.github.com>
2025-10-26 19:26:03 -0300

    atualização 26/10/2025

--- Carmesim_Quest/main.c
+++ Carmesim_Quest/main.c
@@ -6,6 +6,21 @@
 #include <time.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
+
+//estrutura dos personagens
+typedef struct character{
+
+    int HP;
+    int STR;
+    int INT;
+    int DEX;
+    bool vivo;
+    bool mao_esq;
+    bool mao_dir;
+
+}CHC;
+
 
 //funções para texto abaixo
 typedef struct TextTexture{
@@ -68,6 +83,7 @@ void freeTextTexture(TTR text){
     }
 }
 
+
 //funções para imagens abaixo
 SDL_Texture* carregarTextura(const char* caminho, SDL_Renderer* renderer){
 
@@ -101,6 +117,90 @@ OBI criarObjetoImagem(const char* caminho, SDL_Renderer* renderer, int x, int y,
 
 }
 
+
+typedef struct Noimagem{
+
+    OBI imagem;
+    char nome[50];
+    struct Noimagem* prox;
+
+}Noimagem;
+
+typedef struct ListaImagens{
+
+    Noimagem* head;
+    int qtde;
+
+}ListaImagens;
+
+//criar uma nova lista vazia
+ListaImagens* criarListaImagens(){
+
+    ListaImagens* lista = (ListaImagens*)malloc(sizeof(ListaImagens));
+    lista -> head = NULL;
+    lista -> qtde = 0;
+    return lista;
+}
+
+void adicionarImagem(ListaImagens* lista, OBI imagem, const char* nome){
+
+    Noimagem* novoNo = (Noimagem*)malloc(sizeof(Noimagem));
+    novoNo -> imagem = imagem;
+    strcpy(novoNo -> nome, nome);
+    novoNo -> prox = NULL;
+
+    //se a lista estiver vazia
+    if(lista -> head == NULL){
+        lista -> head = novoNo;
+    }
+    else{
+        Noimagem* atual = lista -> head;
+
+        while(atual -> prox != NULL){
+            atual = atual -> prox;
+        }
+        atual -> prox = novoNo;
+    }
+    lista -> qtde++;
+}
+
+OBI* buscaImagem(ListaImagens* lista, const char* nome){
+
+    Noimagem* atual = lista -> head;
+
+    while(atual != NULL){
+        if(strcmp(atual -> nome, nome) == 0){
+            return &(atual -> imagem);
+        }
+        atual = atual -> prox;
+    }
+    return NULL; //caso não encontre
+}
+
+void renderizarImagemPorNome(ListaImagens* lista, SDL_Renderer* renderer, const char* nome){
+
+    OBI* imagem = buscaImagem(lista, nome);
+    if(imagem && imagem -> textura){
+        SDL_RenderCopy(renderer, imagem -> textura, NULL, &imagem -> posicao);
+    }
+}
+
+//liberar toda a memoria da lista
+
+void liberarListaImagem(ListaImagens* lista){
+
+    Noimagem* atual = lista -> head;
+    while(atual != NULL){
+        Noimagem* proximo = atual -> prox;
+        if(atual -> imagem.textura){
+            SDL_DestroyTexture(atual -> imagem.textura);
+        }
+        free(atual);
+        atual = proximo;
+    }
+    free(lista);
+}
+
 int main (int argc, char* args[])
 {
     srand(time(NULL));
@@ -116,21 +216,46 @@ int main (int argc, char* args[])
 
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    //carrgar imagens
+    /*carrgar imagens
     OBI img1 = criarObjetoImagem("teste_fundo_Menu.png", ren, 0, 0, 800, 600);
-    OBI img2 = criarObjetoImagem("vila_place_holder.png", ren, 0, 0, 800, 600);
+    OBI img2 = criarObjetoImagem("vila_place_holder.png", ren, 0, 0, 800, 500);
+    OBI img3 = criarObjetoImagem("planicie_place_holder.png", ren, 0, 0, 800, 500);
+    OBI img4 = criarObjetoImagem("pantano_place_holder.png", ren, 0, 0, 800, 500);*/
+
+    //cria a lista
+    ListaImagens* listaimagens = criarListaImagens();
+
+    //adiciona na lista
+    adicionarImagem(listaimagens, criarObjetoImagem("teste_fundo_Menu.png", ren, 0, 0, 800, 600), "fundo_menu");
+    adicionarImagem(listaimagens, criarObjetoImagem("vila_place_holder.png", ren, 0, 0, 800, 500), "vila");
+    adicionarImagem(listaimagens, criarObjetoImagem("planicie_place_holder.png", ren, 0, 0, 800, 500), "planicie");
+    adicionarImagem(listaimagens, criarObjetoImagem("pantano_place_holder.png", ren, 0, 0, 800, 500), "pantano");
+
 
     // Carregar a fonte uma única vez no início
     TTF_Font* fnt = TTF_OpenFont("minecraft_font.ttf", 12);
     SDL_Color corTexto = { 0, 255, 255, 255};
-    int texto_visivel = 0;
 
+    int texto_visivel1 = 0;
     TTR hello = CarregaTexto(ren, fnt, "Deu certo", corTexto);
     TTR sdl = CarregaTexto(ren, fnt, "SDL2 com texto", corTexto);
+
+    int texto_visivel2 = 0;
+    TTR estafunc = CarregaTexto(ren, fnt, "O texto esta carregando aqui!!", corTexto);
 
     //blocos
     SDL_Rect r = { 0, 500, 200, 200 };
     SDL_Rect r2 = { 600, 500, 200, 200 };
+    SDL_Rect r3 = { 0, 500, 800, 200};
+
+    bool blck1_view = true;
+    bool cfm_click1 = true;
+
+    bool blck2_view = true;
+    bool cfm_click2 = true;
+
+    bool blck3_view = false;
+    bool cfm_click3 = false;
 
     //bloco do fade
 
@@ -138,7 +263,16 @@ int main (int argc, char* args[])
     //permite a aparição da imagem
     bool vrfim1 = true;
     bool vrfim2 = false;
+    bool vrfim3 = false;
 
+    //cria o protagonista
+    CHC prtg;
+    prtg.HP = 20;
+    prtg.STR = 5;
+    prtg.INT = 5;
+    prtg.DEX = 10;
+
+    //gerais
     int stop = 0;
     int espera = 500;
     int mouseX, mouseY;
@@ -183,27 +317,59 @@ int main (int argc, char* args[])
                 SDL_GetMouseState(&mouseX, &mouseY);
 
                 //click em r
-                if(mouseX >= r.x && mouseX <= r.x + r.w && mouseY >= r.y && mouseY <= r.y + r.h){
+                if(cfm_click1 == true){
 
-                    /*printf("\nX:%d, Y:%d", mouseX, mouseY);
-                    red = rand() % 255;
-                    green = rand() % 255;
-                    blue = rand() % 255;
+                    if(mouseX >= r.x && mouseX <= r.x + r.w && mouseY >= r.y && mouseY <= r.y + r.h){
 
-                    r2.x = rand() % 400;
-                    r2.y = rand() % 400;*/
+                        /*printf("\nX:%d, Y:%d", mouseX, mouseY);
+                        red = rand() % 255;
+                        green = rand() % 255;
+                        blue = rand() % 255;
 
-                    printf("\nposição X r2:%d\n", r2.x);
-                    printf("\nposição Y r2:%d\n", r2.y);
+                        r2.x = rand() % 400;
+                        r2.y = rand() % 400;*/
 
+                        printf("\nposição X r2:%d\n", r2.x);
+                        printf("\nposição Y r2:%d\n", r2.y);
+
+                        vrfim1 = false;
+                        vrfim2 = true;
+
+                        blck1_view = false;
+                        cfm_click1 = false;
+
+                        blck2_view = false;
+                        cfm_click2 = false;
+
+                        blck3_view = true;
+                        cfm_click3 = true;
+
+                        texto_visivel2 = 1;
+
+                    }
                 }
 
+
                 //click em r2
-                if(mouseX >= r2.x && mouseX <= r2.x + r2.w && mouseY >= r2.y && mouseY <= r2.y + r2.h){
-                    texto_visivel = 1;
+                if(cfm_click2 == true){
+
+                    if(mouseX >= r2.x && mouseX <= r2.x + r2.w && mouseY >= r2.y && mouseY <= r2.y + r2.h){
+                        texto_visivel1 = 1;
+                    }
+                }
+
+                //click 3:
+                if(cfm_click3 == true){
+
+                    if(mouseX >= r3.x && mouseX <= r3.x + r3.w && mouseY >= r3.y && mouseY <= r3.y + r3.h){
+                        vrfim3 = true;
+                        vrfim2 = false;
+                        vrfim1 = false;
+                    }
                 }
             }
         }
+
         else{
             espera = 500;
         }
@@ -218,38 +384,50 @@ int main (int argc, char* args[])
 
         //renderiza imagem
 
-        if(img1.textura && vrfim1){
-            SDL_RenderCopy(ren, img1.textura, NULL, &img1.posicao);
+        if(vrfim1){
+            renderizarImagemPorNome(listaimagens, ren, "fundo_menu");
         }
 
-        if(img2.textura && vrfim2){
-            SDL_RenderCopy(ren, img2.textura, NULL, &img2.posicao);
+        if(vrfim2){
+            renderizarImagemPorNome(listaimagens, ren, "vila");
         }
 
-        //renderizando texto
-        if(texto_visivel == 1){
-            renderTextAt(ren, hello, 50, 50);
-            renderTextAt(ren, sdl, 50, 100);
+        if(vrfim3){
+            renderizarImagemPorNome(listaimagens, ren, "planicie");
         }
 
         //renderiza bloco
 
-        SDL_SetRenderDrawColor(ren, 0, 0, blue, 0x00);
-        SDL_RenderFillRect(ren, &r);
+        if(blck1_view == true){
+            SDL_SetRenderDrawColor(ren, 0, 0, blue, 0x00);
+            SDL_RenderFillRect(ren, &r);
+        }
 
-        SDL_SetRenderDrawColor(ren, 255, 0, 0, 0);
-        SDL_RenderFillRect(ren, &r2);
+        if(blck2_view == true){
+            SDL_SetRenderDrawColor(ren, 255, 0, 0, 0);
+            SDL_RenderFillRect(ren, &r2);
+        }
+
+        if(blck3_view == true){
+            SDL_SetRenderDrawColor(ren, 0, 0, 0, 0x00);
+            SDL_RenderFillRect(ren, &r3);
+        }
+
+        //renderizando texto
+        if(texto_visivel1 == 1){
+            renderTextAt(ren, hello, 50, 50);
+            renderTextAt(ren, sdl, 50, 100);
+        }
+
+        if(texto_visivel2 == 1){
+            renderTextAt(ren, estafunc, 0, 500);
+        }
 
         SDL_RenderPresent(ren);
     }
 
     //limpar imagens
-    if(img1.textura){
-        SDL_DestroyTexture(img1.textura);
-    }
-    if(img2.textura){
-        SDL_DestroyTexture(img2.textura);
-    }
+    liberarListaImagem(listaimagens);
 
     freeTextTexture(hello);
     freeTextTexture(sdl);
